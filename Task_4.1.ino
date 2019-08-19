0364cce1e944607d1690fa5737ef4fa5183d9cef
vkwatra1711 <52880809+vkwatra1711@users.noreply.github.com>
2019-08-19 19:54:45 +1000

    Add files via upload

--- Task_4.1.ino
+++ Task_4.1.ino
@@ -1,3 +1,11 @@
+#include <RTClib.h>
+
+ // Connect the sensor to the A0(Analog 0) pin on the Arduino board
+ // the sensor value description
+ // 0  ~300     dry soil
+ // 300~700     humid soil
+ // 700~950     in water
+  
 void setup() 
 {
   Serial.begin(9600); // open serial port, set the baud rate as 9600 bps
@@ -8,7 +16,7 @@ void loop()
   int val;
   val = analogRead(0); //connect sensor to Analog 0
   Serial.println(val); //print the value to serial port
-  if ( val <= 100 )
+  if ( val <= 300 )
   {
     digitalWrite(LED_BUILTIN, HIGH);
   }
@@ -16,4 +24,5 @@ void loop()
   {
     digitalWrite(LED_BUILTIN, LOW);
   }
+  delay(3600000);
 }
